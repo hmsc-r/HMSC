e395a3152fdb0b0c7cce34b196d73c3ef81070c1
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2018-04-30 12:41:00 +0300

    Added poolMcmcChains function.

---
+++ R/poolMcmcChains.R
@@ -0,0 +1,18 @@
+#' @title poolMcmcChains
+#'
+#' @description Combines a list MCMC chains into single chain and
+#' @param postList list of posterior chains
+#' @param chainIndex index of chains to be included
+#' @param start index of first MCMC sample included
+#' @param thin thinning between included MCMC samples
+#'
+
+poolMcmcChains = function(postList, chainIndex=1:length(postList), start=1, thin=1){
+   post = list()
+   for(i in chainIndex){
+      chain = postList[[i]]
+      ind = seq(start, length(chain), thin)
+      post = c(post, chain[[ind]])
+   }
+   return(post)
+}
