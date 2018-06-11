342c0d6e3711e6e552b153e9db419a38cf71ab37
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2018-06-11 21:43:17 +0300

    Added getPostEstimate method.

---
+++ R/getPostEstimate.R
@@ -0,0 +1,44 @@
+#' @title Hmsc$getPostEstimate
+#'
+#' @description Calculates mean, support and other posterior quantities for model parameter
+#' @param parName The name of the parameter to be summarized. Can take value of model's baseline parameters, "Omega" or "OmegaCor".
+#' @param r=1 Which random level calculate the parameter for. Has effect only for Eta, Lambda, Omega and OmegaCor.
+#' @param q=c() Which quantiles to calculate.
+#' @param chainIndex=1:length(self$postList) Which posterir chains to use for summarization.
+#'
+#'@examples
+#'
+
+getPostEstimate = function(parName, r=1, q=c(), chainIndex=1:length(self$postList)){
+   m = self
+   bind0 = function(...){
+      abind(...,along=0)
+   }
+   postList = poolMcmcChains(m$postList, chainIndex=chainIndex)
+
+   if(parName %in% c("Beta", "Gamma", "V", "sigma")){
+      valList = lapply(postList, function(a) a[[parName]])
+   }
+   if(parName %in% c("Eta", "Lambda", "Psi", "Delta")){
+      valList = lapply(postList, function(a) a[[parName]][[r]])
+   }
+   if(parName %in% c("Omega", "OmegaCor")){
+      valList = lapply(postList, function(a) crossprod(a[["Lambda"]][[r]]))
+      if(parName %in% c("OmegaCor")){
+         valList = lapply(valList, function(Om) cov2cor(Om))
+      }
+   }
+   val = do.call(bind0, valList)
+   parDim = dim(valList[[1]])
+   parDimLength = length(parDim)
+   valMean = apply(val, 1+(1:parDimLength), mean)
+   valSupport = apply(val>0, 1+(1:parDimLength), mean)
+   res = list(mean=valMean, support=valSupport)
+   if(length(q) > 0)
+      res$q = apply(val, 1+(1:parDimLength), quantile, q)
+   return(res)
+}
+
+Hmsc$set("public", "getPostEstimate", getPostEstimate, overwrite=TRUE)
+
+
