efbfe76ed85f4939c9d414a31267507578e16744
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2018-11-30 09:03:50 +0200

    Fix to computePredictedValues.

--- R/computePredictedValues.R
+++ R/computePredictedValues.R
@@ -15,17 +15,18 @@
 #'
 #' @export
 
-computePredictedValues = function(hM, partition=NULL, start=1, Yc=NULL, mcmcStep=1, expected=TRUE){
+computePredictedValues = function(hM, partition=NULL, start=1, Yc=NULL, mcmcStep=1, expected=TRUE, nCores=1){
    if(is.null(partition)){
-      postList=poolMcmcChains(hM$postList, start = start)
+      postList = poolMcmcChains(hM$postList, start=start)
       pred = predict(hM, post=postList, Yc=Yc, mcmcStep=1, expected=expected)
-      predArray = abind(pred,along=3)
+      predArray = abind(pred, along=3)
    } else{
       if(length(partition) != hM$ny){
          stop("HMSC.computePredictedValues: partition parameter must be a vector of length ny")
       }
       nfolds = length(unique(partition))
-      mpred = matrix(NA,hM$ny,hM$ns)
+      postN = Reduce(sum, lapply(hM$postList, length))
+      predArray = array(NA,c(hM$ny,hM$ns,postN))
       for (k in 1:nfolds){
          print(sprintf("Cross-validation, fold %d out of %d", k, nfolds))
          train = (partition!=k)
@@ -36,21 +37,19 @@ computePredictedValues = function(hM, partition=NULL, start=1, Yc=NULL, mcmcStep
             dfPi[,r] = factor(hM$dfPi[train,r])
          }
          hM1 = Hmsc(Y=as.matrix(hM$Y[train,]), X=as.matrix(hM$X[train,]), Xs=as.matrix(hM$Xs[train,]), Xv=as.matrix(hM$Xv[train,]), dist="probit", studyDesign=dfPi, Tr=hM$Tr, C=hM$C, ranLevels=hM$rL)
-         hM1$distr=hM$distr
+         hM1$distr = hM$distr
          # HOW TO BETTER SET THE DISTRIBUTION?
          # NEED TO INHERIT PRIORS, SCALINGS ETC. FROM hM
-         hM1 = sampleMcmc(hM1, samples=hM$samples, thin=hM$thin, transient=hM$transient,adaptNf=hM$adaptNf, nChains=1)
-         postList = hM1$postList[[1]]
-         postList = postList[start:length(postList)]
+         hM1 = sampleMcmc(hM1, samples=hM$samples, thin=hM$thin, transient=hM$transient, adaptNf=hM$adaptNf, nChains=length(hM$postList), nCores=nCores)
+         postList = poolMcmcChains(hM1$postList, start=start)
          dfPi = as.data.frame(matrix(NA,sum(val),hM$nr))
          colnames(dfPi) = hM$rLNames
          for (r in seq_len(hM$nr)){
             dfPi[,r] = factor(hM$dfPi[val,r])
          }
          pred1 = predict(hM1, post=postList, X=as.matrix(hM$X[val,,drop=FALSE]), studyDesign=dfPi, Yc=Yc[val,,drop=FALSE], mcmcStep=mcmcStep, expected=expected)
-         predArray = abind(pred1,along=3)
+         predArray[val,,] = abind(pred1,along=3)
       }
    }
-   colnames(mpred) = hM$spNames
    return(predArray)
 }
