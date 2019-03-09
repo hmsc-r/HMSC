c9cd6fded25a06de9f960df48f756756e2dabe8c
Otso Ovaskainen <Otso Ovaskainen>
2019-03-09 13:22:51 +0200

    Added the possibility of conditional cross-validation to computePredicted values. Added F1 help for it.
    Fixed predict function for conditional predictions.
    
    Several other minor fixes.

--- R/computePredictedValues.R
+++ R/computePredictedValues.R
@@ -1,10 +1,22 @@
 #' @title computePredictedValues
 #'
 #' @description Computes predicted values from the trained model
+#' @param expected whether expected values (TRUE) or realizations (FALSE) are to be predicted
 #' @param partition vector of observations partitioning for cross-validation
+#' @param partition.sp vector of species partitioning for conditional cross-validation
+#' @param Yc response matrix on which the predictions are to be conditioned on
+#' @param mcmcStep number of MCMC steps used to make conditional predictions
 #' @param start index of first MCMC sample included
 #'
 #'
+#' @details If the option partition is not used, the posterior predictive distribution is based on the model
+#' fitted to the full data. If the option partition is used but partition.sp is not used, the  posterior predictive distribution
+#' is based on cross-validation over the sampling units. If partition.sp is additionally used, when predictions are made for
+#' each fold of the sampling units, the predictions are done separately for each fold of species. When making the predictions
+#' for one fold of species, the predictions are conditional on known occurrences (those observed in the data) of the species
+#' belonging to the other folds. If partition.sp is used, the parameter mcmcStep should be set to high enough value to obtain
+#' appropriate conditional predictions. The option Yc can be used alternatively to partition.sp if the conditioning is to be done
+#' to a fixed set of data (independent on which sampling unit and species the predictions are made for).
 #' @return
 #'
 #'
@@ -13,9 +25,22 @@
 #'
 #' @examples
 #'
+#' preds = computePredictedValues(m)
+#' MF = evaluateModelFit(hM=m, predY=preds)
+#'
+#' partition = createPartition(m, nfolds = 2)
+#' predsCV1 = computePredictedValues(m,partition=partition)
+#' MFCV1 = evaluateModelFit(hM=m, predY=predsCV1)
+#'
+#' predsCV2 = computePredictedValues(m, partition = partition, partition.sp = 1:m$ns, mcmcStep = 100)
+#' MFCV2 = evaluateModelFit(hM=m, predY=predsCV2)
+#'
+#' # Here it is expectd that the measures of model fit be highest for MF,
+#' # second highest for MFCV2, and lowest for MFCV1
+#'
 #' @export
 
-computePredictedValues = function(hM, partition=NULL, start=1, Yc=NULL, mcmcStep=1, expected=TRUE, initPar=NULL, nParallel=1){
+computePredictedValues = function(hM, partition=NULL, partition.sp=NULL, start=1, Yc=NULL, mcmcStep=1, expected=TRUE, initPar=NULL, nParallel=1){
    if(is.null(partition)){
       postList = poolMcmcChains(hM$postList, start=start)
       pred = predict(hM, post=postList, Yc=Yc, mcmcStep=1, expected=expected)
@@ -37,14 +62,14 @@ computePredictedValues = function(hM, partition=NULL, start=1, Yc=NULL, mcmcStep
             dfPi[,r] = factor(hM$dfPi[train,r])
          }
          switch(class(hM$X),
-            matrix = {
-               XTrain = hM$X[train,,drop=FALSE]
-               XVal = hM$X[val,,drop=FALSE]
-            },
-            list = {
-               XTrain = lapply(hM$X, function(a) a[train,,drop=FALSE])
-               XVal = lapply(hM$X, function(a) a[val,,drop=FALSE])
-            }
+                matrix = {
+                   XTrain = hM$X[train,,drop=FALSE]
+                   XVal = hM$X[val,,drop=FALSE]
+                },
+                list = {
+                   XTrain = lapply(hM$X, function(a) a[train,,drop=FALSE])
+                   XVal = lapply(hM$X, function(a) a[val,,drop=FALSE])
+                }
          )
          hM1 = Hmsc(Y=hM$Y[train,,drop=FALSE], X=XTrain, dist="probit", studyDesign=dfPi, Tr=hM$Tr, C=hM$C, ranLevels=hM$rL)
          hM1$distr = hM$distr
@@ -57,8 +82,23 @@ computePredictedValues = function(hM, partition=NULL, start=1, Yc=NULL, mcmcStep
          for (r in seq_len(hM$nr)){
             dfPi[,r] = factor(hM$dfPi[val,r])
          }
-         pred1 = predict(hM1, post=postList, X=XVal, studyDesign=dfPi, Yc=Yc[val,,drop=FALSE], mcmcStep=mcmcStep, expected=expected)
-         predArray[val,,] = abind(pred1,along=3)
+         if(is.null(partition.sp)){
+            pred1 = predict(hM1, post=postList, X=XVal, studyDesign=dfPi, Yc=Yc[val,,drop=FALSE], mcmcStep=mcmcStep, expected=expected)
+            pred1Array = abind(pred1,along=3)
+         } else {
+            pred1Array =  array(dim=c(sum(val),hM$ns,postN))
+            nfolds.sp = length(unique(partition.sp))
+            for (i in 1:nfolds.sp){
+               train.sp = (partition.sp!=i)
+               val.sp = (partition.sp==i)
+               Yc = matrix(NA,nrow=sum(val), ncol=hM$ns)
+               Yc[,train.sp] = hM$Y[val,train.sp,drop=FALSE]
+               pred2 = predict(hM1, post=postList, X=XVal, studyDesign=dfPi, Yc=Yc, mcmcStep=mcmcStep, expected=expected)
+               pred2Array = abind(pred2,along=3)
+               pred1Array[,val.sp,] = pred2Array[,val.sp,]
+            }
+         }
+         predArray[val,,] = pred1Array
       }
    }
    return(predArray)
--- R/predict.R
+++ R/predict.R
@@ -25,34 +25,34 @@
 #' @export
 
 predict.Hmsc = function(hM, post=poolMcmcChains(hM$postList), XData=NULL, X=NULL, # this has to be updated to cov-dependent associations
-   studyDesign=hM$studyDesign, ranLevels=hM$ranLevels,
-   Yc=NULL, mcmcStep=1, expected=FALSE, predictEtaMean=FALSE){
+                        studyDesign=hM$studyDesign, ranLevels=hM$ranLevels,
+                        Yc=NULL, mcmcStep=1, expected=FALSE, predictEtaMean=FALSE){
 
    if(!is.null(XData) && !is.null(X)){
       stop("Hmsc.predict: only single of XData and X arguments can be specified")
    }
    if(!is.null(XData)){
       switch(class(XData),
-         list={
-            xlev = lapply(Reduce(rbind,hM$XData), levels)[unlist(lapply(Reduce(rbind,hM$XData), is.factor))]
-            X = lapply(XData, function(a) model.matrix(hM$XFormula, a, xlev=xlev))
-         },
-         data.frame={
-            xlev = lapply(hM$XData, levels)[unlist(lapply(hM$XData, is.factor))]
-            X = model.matrix(hM$XFormula, XData, xlev=xlev)
-         }
+             list={
+                xlev = lapply(Reduce(rbind,hM$XData), levels)[unlist(lapply(Reduce(rbind,hM$XData), is.factor))]
+                X = lapply(XData, function(a) model.matrix(hM$XFormula, a, xlev=xlev))
+             },
+             data.frame={
+                xlev = lapply(hM$XData, levels)[unlist(lapply(hM$XData, is.factor))]
+                X = model.matrix(hM$XFormula, XData, xlev=xlev)
+             }
       )
    } else{
       if(is.null(X))
          X = hM$X
    }
    switch(class(X),
-      list={
-         nyNew = nrow(X[[1]])
-      },
-      matrix={
-         nyNew = nrow(X)
-      }
+          list={
+             nyNew = nrow(X[[1]])
+          },
+          matrix={
+             nyNew = nrow(X)
+          }
    )
 
    if(!is.null(Yc)){
@@ -76,8 +76,6 @@ predict.Hmsc = function(hM, post=poolMcmcChains(hM$postList), XData=NULL, X=NULL
       dfPiNew = matrix(NA,nyNew,0)
    rL = ranLevels[hM$rLNames]
 
-   # test comment by Oystein
-
    predN = length(post)
    predPostEta = vector("list", hM$nr)
    PiNew = matrix(NA,nrow(dfPiNew),hM$nr)
@@ -85,7 +83,7 @@ predict.Hmsc = function(hM, post=poolMcmcChains(hM$postList), XData=NULL, X=NULL
       postEta = lapply(post, function(c) c$Eta[[r]])
       postAlpha = lapply(post, function(c) c$Alpha[[r]])
       predPostEta[[r]] = predictLatentFactor(unitsPred=levels(dfPiNew[,r]),units=levels(hM$dfPi[,r]),
-         postEta=postEta,postAlpha=postAlpha,rL=rL[[r]],predictMean=predictEtaMean)
+                                             postEta=postEta,postAlpha=postAlpha,rL=rL[[r]],predictMean=predictEtaMean)
       rowNames = rownames(predPostEta[[r]][[1]])
       PiNew[,r] = sapply(dfPiNew[,r], function(s) which(rowNames==s))
    }
@@ -93,14 +91,14 @@ predict.Hmsc = function(hM, post=poolMcmcChains(hM$postList), XData=NULL, X=NULL
    for(pN in 1:predN){
       sam = post[[pN]]
       switch(class(X),
-         matrix = {
-            LFix = X %*% sam$Beta
-         },
-         list = {
-            LFix = matrix(NA,nyNew,hM$ns)
-            for(j in 1:hM$ns)
-               LFix[,j] = X[[j]]%*%sam$Beta[,j]
-         }
+             matrix = {
+                LFix = X %*% sam$Beta
+             },
+             list = {
+                LFix = matrix(NA,nyNew,hM$ns)
+                for(j in 1:hM$ns)
+                   LFix[,j] = X[[j]]%*%sam$Beta[,j]
+             }
       )
       LRan = vector("list",hM$nr)
       Eta = vector("list",hM$nr)
@@ -112,21 +110,21 @@ predict.Hmsc = function(hM, post=poolMcmcChains(hM$postList), XData=NULL, X=NULL
 
       if(!is.null(Yc) && any(!is.na(Yc))){
          Z = L
-         Z = updateZ(Y=Yc,Z=Z,Beta=sam$Beta,iSigma=iSigma,Eta=Eta,Lambda=sam$Lambda, X=X,Pi=PiNew,distr=hM$distr)
+         Z = updateZ(Y=Yc,Z=Z,Beta=sam$Beta,iSigma=1/sam$sigma,Eta=Eta,Lambda=sam$Lambda, X=X,Pi=PiNew,distr=hM$distr,rL=rL)
          for(sN in seq_len(mcmcStep)){
-            Eta = updateEta(Y=Yc,Z=Z,Beta=sam$Beta,iSigma=iSigma,Eta=Eta,Lambda=sam$Lambda,Alpha=sam$Alpha, rLPar=hM$rLPar, X=X,Pi=PiNew,rL=rL)
-            Z = updateZ(Y=Yc,Z=Z,Beta=sam$Beta,iSigma=iSigma,Eta=Eta,Lambda=sam$Lambda, X=X,Pi=PiNew,distr=hM$distr)
+            Eta = updateEta(Y=Yc,Z=Z,Beta=sam$Beta,iSigma=1/sam$sigma,Eta=Eta,Lambda=sam$Lambda,Alpha=sam$Alpha, rLPar=hM$rLPar, X=X,Pi=PiNew,rL=rL)
+            Z = updateZ(Y=Yc,Z=Z,Beta=sam$Beta,iSigma=1/sam$sigma,Eta=Eta,Lambda=sam$Lambda, X=X,Pi=PiNew,distr=hM$distr,rL=rL)
          }
          for(r in seq_len(hM$nr)){
             LRan[[r]] = predPostEta[[r]][[pN]][dfPiNew[,r],] %*% sam$Lambda[[r]]
          }
          if(hM$nr > 0){L = LFix + Reduce("+", LRan)} else L = LFix
-      }
-
-      if(!expected){
-         Z = L + matrix(sqrt(sam$sigma),nrow(L),hM$ns,byrow=TRUE) * matrix(rnorm(nrow(L)*hM$ns),nrow(L),hM$ns)
-      } else{
-         Z = L
+      } else {
+         if(!expected){
+            Z = L + matrix(sqrt(sam$sigma),nrow(L),hM$ns,byrow=TRUE) * matrix(rnorm(nrow(L)*hM$ns),nrow(L),hM$ns)
+         } else{
+            Z = L
+         }
       }
 
       for(j in 1:hM$ns){
@@ -160,5 +158,3 @@ predict.Hmsc = function(hM, post=poolMcmcChains(hM$postList), XData=NULL, X=NULL
    return(pred)
 }
 
-# a test change
-# a second test change
