1ffd15016d7abf3dd2d656c857a2ece1368d866f
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2019-06-10 20:25:08 +0300

    Fix for improved robustm´ness of alignPosterior

--- R/alignPosterior.R
+++ R/alignPosterior.R
@@ -14,29 +14,34 @@ alignPosterior=function(hM){
    ncNRRR = hM$ncNRRR
    nr = hM$nr
 
-   if(nr>0){
-      for (r in 1:nr){
-          for (i in 1:length(hM$postList)){
-            cpL= hM$postList[[i]]
-            valList=lapply(cpL, function(a) a[["Lambda"]][[1]])
-            if (i==1){
-               val = do.call(bind0, valList)
-               valMean = colMeans(val)
-            }
-            s=NULL
-            for(k in 1:dim(valMean)[1]){
-               s=rbind(s,sign(abind(lapply(valList, function(a) cor(as.vector(valMean[k,]),as.vector(a[k,]))))))
-            }
-            for(j in 1:length(cpL)){
-               for(k in 1:dim(valMean)[1]){
-                  if(s[k,j]<0){
-                     cpL[[j]]$Lambda[[1]][k,] = -cpL[[j]]$Lambda[[1]][k,]
-                     cpL[[j]]$Eta[[1]][,k] = -cpL[[j]]$Eta[[1]][,k]
-                  }
+   for(r in seq_len(nr)){
+      nfVec = unlist(lapply(hM$postList, function(postChain) dim(postChain[[1]][["Lambda"]][[r]])[1]))
+      nfMax = max(nfVec)
+      cIndNfMax = which.max(nfVec)
+      cpL = hM$postList[[cIndNfMax]]
+      LambdaPostList = lapply(cpL, function(a) a[["Lambda"]][[r]])
+      LambdaPostArray = do.call(bind0, LambdaPostList)
+      LambdaPostMean = colMeans(LambdaPostArray)
+      for(cInd in 1:length(hM$postList)){
+         cpL = hM$postList[[cInd]]
+         LambdaPostList = lapply(cpL, function(a) a[["Lambda"]][[r]])
+         nf = nrow(LambdaPostList[[1]])
+         s = NULL
+         for(k in 1:nf){
+            if(hM$ns > 1 || hM$rL[[r]]$xDim > 1){
+               s = rbind(s,sign(abind(lapply(LambdaPostList, function(a) cor(as.vector(LambdaPostMean[k,]),as.vector(a[k,]))))))
+            } else
+               s = rbind(s,abind(lapply(LambdaPostList, function(a) sign(LambdaPostMean[k,])*sign(a[k,]))))
+         }
+         for(j in 1:length(cpL)){
+            for(k in 1:nf){
+               if(s[k,j]<0){
+                  cpL[[j]]$Lambda[[r]][k,] = -cpL[[j]]$Lambda[[r]][k,]
+                  cpL[[j]]$Eta[[r]][,k] = -cpL[[j]]$Eta[[r]][,k]
                }
             }
-            hM$postList[[i]] = cpL
          }
+         hM$postList[[cInd]] = cpL
       }
    }
    if(ncRRR>0){
