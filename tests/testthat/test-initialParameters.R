3713db8aa639dc064860d17271d0a76dad62c495
Melinda de Jonge <m.dejonge@fnwi.ru.nl>
2019-09-22 21:23:49 +0200

    Added tests for updaters and initial parameters

---
+++ tests/testthat/test-initialParameters.R
@@ -0,0 +1,186 @@
+context('Test computeInitialParameters with standard settings')
+
+test_that("computeInitialParameters gives right number of output parameters",{
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList),12)
+})
+
+test_that("Standard initial beta is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList$Beta),12)
+   expect_equal(round(parList$Beta),t(matrix(c(-1,-1,-1,0,0,-0,0,-1,1,1,1,1),nrow=4,ncol=3)))
+})
+
+test_that("Standard initial gamma is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList$Gamma),9)
+   expect_equal(round(parList$Gamma),t(matrix(c(0,1,-1,0,0,0,0,0,0),nrow=3,ncol=3)))
+})
+
+test_that("Standard initial V is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList$V),9)
+   expect_equal(round(parList$V),t(matrix(c(0,0,0,0,1,0,0,0,0),nrow=3,ncol=3)))
+})
+
+test_that("Standard initial Sigma is correct",{
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(parList$sigma,rep(1,4))
+})
+
+test_that("Standard initial Eta is distributed as N(0,1)",{
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList$Eta),2)
+   expect_equal(length(parList$Eta[[1]]),100)
+   expect_equal(length(parList$Eta[[2]]),20)
+   expect_equal(round(sd(parList$Eta[[1]])),1)
+   expect_equal(round(sd(parList$Eta[[2]])),1)
+   expect_equal(round(mean(parList$Eta[[1]])),0)
+   expect_equal(round(mean(parList$Eta[[2]])),0)
+})
+
+test_that("Standard initial Alpha is correct",{
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList$Alpha),2)
+   expect_equal(length(parList$Alpha[[1]]),2)
+   expect_equal(length(parList$Alpha[[2]]),2)
+   expect_equal(parList$Alpha[[1]],c(1,1))
+   expect_equal(parList$Alpha[[2]],c(1,1))
+})
+
+test_that("Standard initial Rho is correct",{
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(parList$rho,1)
+})
+
+test_that("Standard initial Lambda is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList$Lambda),2)
+   expect_equal(length(parList$Lambda[[1]]),8)
+   expect_equal(length(parList$Lambda[[2]]),8)
+   expect_equal(round(parList$Lambda[[1]]),matrix(rep(0,8),nrow=2,ncol=4))
+   expect_equal(round(parList$Lambda[[2]]),matrix(rep(0,8),nrow=2,ncol=4))
+})
+
+test_that("Standard initial Psi is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList$Psi),2)
+   expect_equal(length(parList$Psi[[1]]),8)
+   expect_equal(length(parList$Psi[[2]]),8)
+   expect_equal(round(parList$Psi[[1]]),t(matrix(c(0,1,0,1,1,0,0,1),nrow=4,ncol=2)))
+   expect_equal(round(parList$Psi[[2]]),t(matrix(c(1,2,0,3,1,2,1,0),nrow=4,ncol=2)))
+})
+
+test_that("Standard initial Delta is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(length(parList$Delta),2)
+   expect_equal(length(parList$Delta[[1]]),2)
+   expect_equal(length(parList$Delta[[2]]),2)
+   expect_equivalent(round(parList$Delta[[1]]),c(42,51))
+   expect_equivalent(round(parList$Delta[[2]]),c(46,53))
+})
+
+test_that("Standard initial Z is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   expect_equal(nrow(parList$Z),TD$units)
+   expect_equal(ncol(parList$Z),TD$ns)
+   expect_equal(round(colMeans(parList$Z)),c(-1,0,0,0))
+})
+
+
+## -----------------------------------------------------------------------------
+context('Test computeInitialParameters based on intial parameters from fixed effects models')
+
+test_that("Fixed effects initial beta is correct",{
+   set.seed(200)
+   Y=matrix(1:20,nrow=5,ncol=4)
+   Y[,2]= sample(c(0,1),5,replace = TRUE)
+   m = Hmsc(Y=Y,XData=data.frame(x1 = 1:5),distr = c('normal','probit','poisson','lognormal poisson'))
+   parList = computeInitialParameters(m,initPar='fixed effects')
+   fit1 = lm.fit(m$XScaled,m$Y[,1])
+   fit2 = glm.fit(m$XScaled,m$Y[,2],family=binomial(link="probit"))
+   fit3 = glm.fit(m$XScaled,m$Y[,3],family=poisson())
+   fit4 = glm.fit(m$XScaled,m$Y[,4],family=poisson())
+   expect_equivalent(parList$Beta[,1],fit1$coefficients)
+   expect_equivalent(parList$Beta[,2],fit2$coefficients)
+   expect_equivalent(parList$Beta[,3],fit3$coefficients)
+   expect_equivalent(parList$Beta[,4],fit4$coefficients)
+})
+
+test_that("Fixed effects initial gamma",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar='fixed effects')
+   fit1 = lm.fit(TD$m$Tr,parList$Beta[1,])
+   fit2 = lm.fit(TD$m$Tr,parList$Beta[2,])
+   fit3 = lm.fit(TD$m$Tr,parList$Beta[3,])
+   expect_equivalent(parList$Gamma[1,],fit1$coefficients)
+   expect_equivalent(parList$Gamma[2,],fit2$coefficients)
+   expect_equivalent(parList$Gamma[3,],fit3$coefficients)
+})
+
+test_that("Fixed effects initial V",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar='fixed effects')
+   expect_equal(round(parList$Gamma),t(matrix(c(-4,2,4,1,0,0,0,-2,-1),nrow=3,ncol=3)))
+})
+
+## -----------------------------------------------------------------------------
+context('Test computeDataParameters')
+
+test_that("computeDataParameters gives right number of parameters",{
+   parList = computeDataParameters(TD$m)
+   expect_equal(length(parList),5)
+   expect_equal(length(parList$rLPar[[1]]),0)
+   expect_equal(length(parList$rLPar[[2]]),4)
+})
+
+test_that("phylogenetic parameters when phylogeny is provided",{
+   set.seed(200)
+   parList = computeDataParameters(TD$m)
+   expect_equal(length(parList$detQg),nrow(TD$m$rhopw))
+   expect_equal(round(sum(parList$detQg)),-68)
+   expect_equal(dim(parList$Qg),c(TD$m$ns,TD$m$ns,nrow(TD$m$rhopw)))
+   expect_equal(dim(parList$iQg),c(TD$m$ns,TD$m$ns,nrow(TD$m$rhopw)))
+   expect_equal(dim(parList$RQg),c(TD$m$ns,TD$m$ns,nrow(TD$m$rhopw)))
+   expect_equal(round(sum(parList$Qg)),575)
+   expect_equal(round(sum(parList$iQg)),293)
+   expect_equal(round(sum(parList$RQg)),461)
+})
+
+test_that("phylogenetic parameters when phylogeny is not provided",{
+   m = Hmsc(Y=matrix(1:20,nrow=10,ncol=2),X=matrix(1:20,nrow=10,ncol=2))
+   parList = computeDataParameters(m)
+   expect_equal(parList$detQg,0)
+   expect_equal(dim(parList$Qg),c(m$ns,m$ns,1))
+   expect_equal(dim(parList$iQg),c(m$ns,m$ns,1))
+   expect_equal(dim(parList$RQg),c(m$ns,m$ns,1))
+   expect_equal(parList$Qg[,,1],matrix(c(1,0,0,1),nrow=2,ncol=2))
+   expect_equal(parList$iQg[,,1],matrix(c(1,0,0,1),nrow=2,ncol=2))
+   expect_equal(parList$RQg[,,1],matrix(c(1,0,0,1),nrow=2,ncol=2))
+})
+
+test_that("spatial parameters when spatial data is not provided",{
+   m = Hmsc(Y=matrix(1:20,nrow=10,ncol=2),X=matrix(1:20,nrow=10,ncol=2))
+   parList = computeDataParameters(m)
+   expect_equal(length(parList$rLPar),0)
+})
+
+test_that("spatial parameters when spatial data is provided",{
+   set.seed(200)
+   parList = computeDataParameters(TD$m)
+   expect_equal(length(parList$detQg),nrow(TD$m$rhopw))
+   expect_equal(round(sum(parList$rLPar[[2]]$detWg)),-601)
+   expect_equivalent(dim(parList$rLPar[[2]]$Wg),c(TD$plots,TD$plots,nrow(TD$m$rL[[2]]$alphapw)))
+   expect_equivalent(dim(parList$rLPar[[2]]$iWg),c(TD$plots,TD$plots,nrow(TD$m$rL[[2]]$alphapw)))
+   expect_equivalent(dim(parList$rLPar[[2]]$RiWg),c(TD$plots,TD$plots,nrow(TD$m$rL[[2]]$alphapw)))
+   expect_equal(round(sum(parList$rLPar[[2]]$Wg)),4620)
+   expect_equal(round(sum(parList$rLPar[[2]]$iWg)),329)
+   expect_equal(round(sum(parList$rLPar[[2]]$RiWg)),476)
+})
---
+++ tests/testthat/test-sampling.R
@@ -0,0 +1,182 @@
+context('Test updaters')
+
+test_that("updateGamma2 is correct", {
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   dataParList = computeDataParameters(TD$m)
+   Gamma = updateGamma2(Z=parList$Z,Gamma=parList$Gamma,iV=chol2inv(chol(parList$V)),iSigma=sqrt(parList$sigma),
+                        Eta=parList$Eta,Lambda=parList$Lambda, X=TD$m$X,Pi=TD$m$Pi,dfPi=TD$m$dfPi,Tr=TD$m$Tr,
+                        C=TD$m$C,rL=TD$m$rL, iQg=dataParList$iQg, mGamma=TD$m$mGamma,iUGamma=chol2inv(chol(TD$m$UGamma)))
+   expect_equal(ncol(Gamma),3)
+   expect_equal(nrow(Gamma),3)
+   expect_equal(round(sum(Gamma)),0)
+})
+
+test_that("UpdateGammaEta is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   dataParList = computeDataParameters(TD$m)
+   GammaEtaList = updateGammaEta(Z=parList$Z,Gamma=parList$Gamma,V=parList$V,iV=chol2inv(chol(parList$V)),id=sqrt(parList$sigma),
+                                 Eta=parList$Eta,Lambda=parList$Lambda,Alpha=parList$Alpha, X=TD$m$X,Pi=TD$m$Pi,
+                                 dfPi=TD$m$dfPi,Tr=TD$m$Tr,rL=TD$m$rL, rLPar=dataParList$rLPar,Q=dataParList$Qg[,,parList$rho],
+                                 iQ=dataParList$iQg[,,parList$rho],RQ=dataParList$RQg[,,parList$rho],
+                                 U=TD$m$UGamma,iU=chol2inv(chol(TD$m$UGamma)))
+   gamma = GammaEtaList$Gamma
+   eta = GammaEtaList$Eta
+   expect_equal(length(eta),2)
+   expect_equal(length(eta[[1]]),100)
+   expect_equal(length(eta[[2]]),20)
+   expect_equal(round(sum(eta[[1]])),-29)
+   expect_equal(round(sum(eta[[2]])),2)
+   expect_equal(length(gamma),9)
+   expect_equal(round(sum(gamma)),0)
+})
+
+test_that("updateBetaLambda is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   dataParList = computeDataParameters(TD$m)
+   BetaLambdaList = updateBetaLambda(Y=TD$Y,Z=parList$Z,Gamma=parList$Gamma,iV=chol2inv(chol(parList$V)),
+                                     iSigma=sqrt(parList$sigma),Eta=parList$Eta,Psi=parList$Psi,Delta=parList$Delta,
+                                     iQ=dataParList$iQg[,,parList$rho],X=TD$m$X,Tr=TD$m$Tr,Pi=TD$m$Pi,dfPi=TD$m$dfPi,C=TD$m$C,rL=TD$m$rL)
+   Beta = BetaLambdaList$Beta
+   Lambda = BetaLambdaList$Lambda
+   expect_equal(length(Lambda),2)
+   expect_equal(length(Lambda[[1]]),8)
+   expect_equal(length(Lambda[[2]]),8)
+   expect_equal(round(sum(Lambda[[1]]),digits=1),0.3)
+   expect_equal(round(sum(Lambda[[2]]),digits=1),0.3)
+   expect_equal(length(Beta),12)
+   expect_equal(round(sum(Beta)),0)
+})
+
+test_that("updateGammaV is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   dataParList = computeDataParameters(TD$m)
+   GammaVList = updateGammaV(Beta=parList$Beta,Gamma=parList$Gamma,iV=chol2inv(chol(parList$V)),rho=TD$m$rho,
+                             iQg=dataParList$iQg,RQg=dataParList$RQg, Tr=TD$m$Tr,C=TD$m$C, mGamma=TD$m$mGamma,
+                             iUGamma=chol2inv(chol(TD$m$UGamma)),V0=TD$m$V0,f0=TD$m$f0)
+   Gamma = GammaVList$Gamma
+   iV = GammaVList$iV
+   expect_equal(nrow(iV),3)
+   expect_equal(ncol(iV),3)
+   expect_equal(round(sum(iV)),18)
+   expect_equal(nrow(Gamma),3)
+   expect_equal(ncol(Gamma),3)
+   expect_equal(round(sum(Gamma)),1)
+})
+
+test_that("updateRho is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   dataParList = computeDataParameters(TD$m)
+   rho = updateRho(Beta=parList$Beta,Gamma=parList$Gamma,iV=chol2inv(chol(parList$V)), RQg=dataParList$RQg,
+                   detQg=dataParList$detQg, Tr=TD$m$Tr, rhopw=TD$m$rhopw)
+   expect_equal(rho,1)
+})
+
+test_that("updateLambdaPriors is correct",{
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   PsiDeltaList = updateLambdaPriors(Lambda=parList$Lambda,Delta=parList$Delta, rL=TD$m$rL)
+   Psi = PsiDeltaList$Psi
+   Delta = PsiDeltaList$Delta
+   expect_equal(length(Delta),2)
+   expect_equivalent(round(Delta[[1]]),c(47,50))
+   expect_equivalent(round(Delta[[2]]),c(59,43))
+   expect_equal(round(sum(Psi[[1]])),11)
+   expect_equal(round(sum(Psi[[2]])),12)
+})
+
+
+test_that("updateEta is correct", {
+   set.seed(200)
+   parList = computeInitialParameters(TD$m,initPar=NULL)
+   dataParList = computeDataParameters(TD$m)
+   eta = updateEta(Y = TD$m$Y, Z=parList$Z,Beta=parList$Beta,iSigma=sqrt(parList$sigma),Eta=parList$Eta,
+             Lambda=parList$Lambda, Alpha = parList$Alpha, rLPar = dataParList$rLPar, X = TD$m$X, Pi = TD$m$Pi,
+             dfPi = TD$m$dfPi,rL=TD$m$rL)
+   expect_equal(length(eta),2)
+   expect_equal(length(eta[[1]]),100)
+   expect_equal(length(eta[[2]]),20)
+   expect_equal(round(sum(eta[[1]])),-10)
+   expect_equal(round(sum(eta[[2]])),-10)
+})
+
+test_that("updateAlpha is correct",{
+   set.seed(200)
+   dataParList = computeDataParameters(TD$m)
+   Alpha = updateAlpha(Eta=TD$m$postList[[1]][[1]]$Eta, rLPar=dataParList$rLPar, rL=TD$m$rL)
+   expect_equal(length(Alpha),2)
+   expect_equal(length(Alpha[[1]]),2)
+   expect_equal(length(Alpha[[2]]),2)
+   expect_equal(Alpha[[1]],c(1,1))
+   expect_equal(Alpha[[2]],c(19,1))
+})
+
+test_that("updateInvSigma is correct",{
+   set.seed(200)
+   Y=matrix(1:20,nrow=5,ncol=4)
+   Y[,2]= sample(c(0,1),5,replace = TRUE)
+   m = Hmsc(Y=Y,XData=data.frame(x1 = 1:5),distr = c('normal','probit','poisson','lognormal poisson'))
+   parList = computeInitialParameters(m,initPar='fixed effects')
+   dataParList = computeDataParameters(m)
+   iSigma = updateInvSigma(Y=m$Y,Z=parList$Z,Beta=parList$Beta,iSigma=sqrt(parList$sigma),
+                           Eta=parList$Eta,Lambda=parList$Lambda, distr=m$distr,X=m$X,Pi=m$Pi,
+                           dfPi=m$dfPi,rL=m$rL, aSigma=m$aSigma,bSigma=m$bSigma)
+   expect_equal(round(iSigma,digits=1),c(0.1,1.0,0.1,0.3))
+})
+
+test_that("updateNf is correct",{
+   set.seed(200)
+   rL2 = HmscRandomLevel(units = TD$studyDesign$sample)
+   m = Hmsc(Y=TD$Y,
+               XData=TD$X,
+               XFormula=~x1+x2,
+               TrData=TD$Tr,
+               TrFormula = ~T1 + T2,
+               phyloTree=TD$phy,
+               ranLevels=list("sample"=rL2),
+               studyDesign = TD$studyDesign,
+               distr=c('probit'))
+   parList = computeInitialParameters(m,initPar=NULL)
+   dataParList = computeDataParameters(m)
+   listPar = updateNf(eta=parList$Eta[[1]],lambda=parList$Lambda[[1]],alpha=parList$Alpha[[1]],psi=parList$Psi[[1]],
+                      delta=parList$Delta[[1]],rL=m$rL[[1]], iter=100)
+   Lambda = listPar$lambda
+   Eta = listPar$eta
+   Alpha = listPar$alpha
+   Psi = listPar$psi
+   Delta = listPar$delta
+   expect_equal(nrow(Lambda),3)
+   expect_equal(ncol(Eta),3)
+   expect_equal(length(Alpha),3)
+   expect_equal(nrow(Psi),3)
+   expect_equal(length(Delta),3)
+})
+## -----------------------------------------------------------------------------
+context('Test sampleMCMC')
+
+test_that("sampleMCMC returns m object of right size",{
+   set.seed(200)
+   m = sampleMcmc(TD$m,samples=1)
+   expect_equal(length(m),72)
+   expect_equal(length(m$postList[[1]][[1]]),13)
+})
+
+test_that("sampleMCMC gives right output", {
+   set.seed(200)
+   m = sampleMcmc(TD$m,samples=1)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Beta)),-5)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Gamma)),-1)
+   expect_equal(round(sum(m$postList[[1]][[1]]$rho)),1)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Eta[[1]])),20)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Eta[[2]])),3)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Lambda[[1]])),-1)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Lambda[[2]])),0)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Psi[[1]])),7)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Psi[[2]])),6)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Delta[[1]])),104)
+   expect_equal(round(sum(m$postList[[1]][[1]]$Delta[[2]])),84)
+   })
