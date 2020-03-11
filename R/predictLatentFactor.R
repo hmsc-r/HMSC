cd3d80d0f12ee819a4664f2b415ef083b799b4fa
Melinda de Jonge <m.dejonge@fnwi.ru.nl>
2020-03-11 22:15:30 +0100

    Minor fix for predictions to 1 unit with nngp

--- R/predictLatentFactor.R
+++ R/predictLatentFactor.R
@@ -119,7 +119,7 @@ predictLatentFactor = function(unitsPred, units, postEta, postAlpha, rL, predict
                          unitsAll = c(units,unitsPred[indNew])
                          s = rL$s[unitsAll,]
                          sOld = s[1:np,]
-                         sNew = s[np+(1:nn),]
+                         sNew = matrix(s[np+(1:nn),],ncol=ncol(sOld))
                          indNN = knnx.index(sOld,sNew,k=rL$nNeighbours)
                          indices = list()
                          dist12 = matrix(NA,nrow=rL$nNeighbours,ncol=nn)
