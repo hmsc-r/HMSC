6ab482b38da2c4c336e488665d24a2b5c9a49e80
gtikhonov <gleb.tikhonov@helsinki.fi>
2020-12-08 22:37:31 +0200

    Fixed integration inconsistency. Fixed alighning factors across chains so that added extra loadings' prior is set to the mode.

--- R/updateLatentLoadingOrder.R
+++ R/updateLatentLoadingOrder.R
@@ -32,30 +32,31 @@ updateLatentLoadingOrder = function(eta,lambda,alpha,delta, rL){
       tauLogMat = array(rep(tauLog,each=N^2),c(N,N,nf))
       tauLogMat[,,h:(h+1)] = tauLog[h]-deltaLog[h]+tauLogGrid
       deltaLogMat = abind(tauLogMat[,,1], aperm(array(apply(tauLogMat,c(1,2),diff),c(nf-1,N,N)), c(2,3,1)), along=3)
-      tmp = array(exp(tauLogMat[,,h:(h+1)]), c(N,N,2,ns))
+      tmp = array(exp(0.5*tauLogMat[,,h:(h+1)]), c(N,N,2,ns))
       Lambda0Mat = array(rep(lambda[h:(h+1),],each=N^2), c(N,N,2,ns)) * tmp
       Lambda0Mat_swap = array(rep(lambda[(h+1):h,],each=N^2), c(N,N,2,ns)) * tmp
       deltaMat = exp(deltaLogMat)
       log_prior_delta = rowSums(dgamma(deltaMat,aArray,bArray,log=TRUE), dims=2) + rowSums(deltaLogMat, dims=2)
       log_prior_Lambda = rowSums(dt(Lambda0Mat,nu,log=TRUE), dims=2)
       log_prior_Lambda_swap = rowSums(dt(Lambda0Mat_swap,nu,log=TRUE), dims=2)
-      log_prior_loading = log_prior_delta+log_prior_Lambda
-      log_prior_loading_swap = log_prior_delta+log_prior_Lambda_swap
+      logSpaceWarp = 0.5*ns*deltaLogGrid[,,1] + 0.5*ns*(deltaLogGrid[,,1]+deltaLogGrid[,,2])
+      log_prior_loading = log_prior_delta + log_prior_Lambda + logSpaceWarp
+      log_prior_loading_swap = log_prior_delta + log_prior_Lambda_swap + logSpaceWarp
       log_prob = logSumExp(log_prior_loading) - 2*log(N)
       log_prob_swap = logSumExp(log_prior_loading_swap) - 2*log(N)
       swapRatio = 1 / (exp(log_prob-log_prob_swap) + 1)
       if(runif(1) <= swapRatio){
          cat(sprintf("swapping %d %d, prob=%.4f\n", h, h+1, swapRatio))
          ind = sample(0:(N^2-1), 1, prob=exp(log_prior_loading_swap-max(log_prior_loading_swap)))
-         ind1 = ind%/%N + 1
-         ind2 = ind%%N + 1
+         ind1 = ind%%N + 1
+         ind2 = ind%/%N + 1
          lambda[h:(h+1),] = lambda[(h+1):h,]
          eta[,h:(h+1)] = eta[,(h+1):h]
          alpha[h:(h+1)] = alpha[(h+1):h]
       } else{
          ind = sample(0:(N^2-1), 1, prob=exp(log_prior_loading-max(log_prior_loading)))
-         ind1 = ind%/%N + 1
-         ind2 = ind%%N + 1
+         ind1 = ind%%N + 1
+         ind2 = ind%/%N + 1
       }
       deltaLog = deltaLogMat[ind1,ind2,]
    }
