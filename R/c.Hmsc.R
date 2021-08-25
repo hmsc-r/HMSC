88d467cb4dcb442a8bdd43366657b7a1b6de79a5
Jari Oksanen <jhoksane@gmail.com>
2021-08-25 16:57:17 +0300

    ignore silently non-sampled Hmsc models without postList

--- R/c.Hmsc.R
+++ R/c.Hmsc.R
@@ -32,18 +32,18 @@
     if (!all(sapply(hMList, inherits, what = "Hmsc")))
         stop("all elements should be Hmsc objects")
     ## all Hmsc object have identical Calls (can give false alarms)
-    tmp <- sapply(hMList, getCall)
+    tmp <- lapply(hMList, getCall)
     if (!all(sapply(tmp, identical, y = tmp[[1]])))
         warning("not all elements have identical function Call")
     ## all chains should be same size
-    tmp <- sapply(hMList, function(x) x$sample)
+    tmp <- unlist(lapply(hMList, function(x) x$samples))
     if (!all(tmp[1] == tmp))
         warning("chains had different lengths: ", paste(tmp, collapse = ", "))
     ## all chains should have same thins
-    tmp <- sapply(hMList, function(x) x$thin)
+    tmp <- unlist(lapply(hMList, function(x) x$thin))
     if (!all(tmp[1] == tmp))
         warning("chains had different thins: ", paste(tmp, collapse = ", "))
-    tmp <- sapply(hMList, function(x) x$transient)
+    tmp <- unlist(lapply(hMList, function(x) x$transient))
     if (!all(tmp[1] == tmp))
         warning("chains had different transients: ", paste(tmp, collapse = ", "))
     ## extract postLists
