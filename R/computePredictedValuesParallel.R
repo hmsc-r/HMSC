50fce7f6bd9d5a9bdaa35211f1426e7e7a14ce48
Jari Oksanen <jhoksane@gmail.com>
2022-01-21 13:12:51 +0200

    naive implementation of model setting
    Scales need be set like in the full data

--- R/computePredictedValuesParallel.R
+++ R/computePredictedValuesParallel.R
@@ -25,7 +25,8 @@
     if (!missing(partition.sp))
         .NotYetUsed("partition.sp", error = TRUE)
     ## STAGE 1: Basic housekeeping
-    nfolds <- length(unique(partition))
+    parts <- unique(partition)
+    nfolds <- length(parts)
     if (thin > 1 || start > 1)
         postN <- sum(sapply(hM$postList, function(z)
             length(seq(from=start, to=length(z), by=thin))))
@@ -34,8 +35,23 @@
     ## output array
     predArray <- array(NA, c(hM$ny, hM$ns, postN))
     ## STEP 1: define new Hmsc model for each nfolds partition
+    ## only implement for the simple case first
+    if (is.list(hM$X))
+        stop("not yet implemented for a list of model matrices")
+    if (hM$ncRRR > 0)
+        stop("not yet implemendted for RRR models")
+    ## no need to parallelism in defining model
+    setHmsc <- function(k, hM) {
+        Hmsc(Y = hM$Y[k == partition, , drop=FALSE],
+             X = hM$X[k == partition,, drop=FALSE],
+             distr = hM$distr,
+             studyDesign = hM$dfPi[k == partition,, drop=FALSE],
+             Tr = hM$Tr, C = hM$C, ranLevels = hM$rL)
+    }
+    hM1 <- lapply(parts, function(k) setHmsc(k, hM))
+
     ## STEP 2: sample Hmsc models for each nfolds * nChains case
     ## STEP 3: combine predictions
     ## OUT
-    predArray
+    hM1
 }
