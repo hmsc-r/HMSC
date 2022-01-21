02e6343c92422977cb50a35858473fa53b374541
Jari Oksanen <jhoksane@gmail.com>
2022-01-21 14:32:34 +0200

    prelude to parallel processing: up to sampling each chain x fold
    - replaces all loops with lapply that can be replaced with
      parallel lapply: still non-parallel
    - only handles simple basic case: no lists of X, nor RRR
    - step 1: sets an Hmsc model for each fold
    - step 2: samples each fold x chain (still non-parallel)

--- R/computePredictedValuesParallel.R
+++ R/computePredictedValuesParallel.R
@@ -40,18 +40,48 @@
         stop("not yet implemented for a list of model matrices")
     if (hM$ncRRR > 0)
         stop("not yet implemendted for RRR models")
-    ## no need to parallelism in defining model
+    ## Pack setting training model into one function
     setHmsc <- function(k, hM) {
-        Hmsc(Y = hM$Y[k == partition, , drop=FALSE],
-             X = hM$X[k == partition,, drop=FALSE],
-             distr = hM$distr,
-             studyDesign = hM$dfPi[k == partition,, drop=FALSE],
-             Tr = hM$Tr, C = hM$C, ranLevels = hM$rL)
+        train <- k != partition
+        m <- Hmsc(Y = hM$Y[train, , drop=FALSE],
+                  X = hM$X[train, , drop=FALSE],
+                  distr = hM$distr,
+                  studyDesign = droplevels(hM$dfPi[train,, drop=FALSE]),
+                  Tr = hM$Tr, C = hM$C, ranLevels = hM$rL)
+        ## old code calls here setPriors, but that does nothing as its
+        ## result is not saved, and it is currently skipped: CHECK
+        ## THIS!
+        m$YScalePar <- hM$YScalePar
+        m$Yscaled <- sweep(m$Y, 2, m$YScalePar[1,]/m$YScalePar[2,])
+        m$XInterceptInd <- hM$XInterceptInd
+        m$XScalePar <- hM$XScalePar
+        m$XScaled <- sweep(m$X, 2, m$XScalePar[1,]/m$XScalePar[2,])
+        m$TrInterceptInd <- hM$TrInterceptInd
+        m$TrScalePar <- hM$TrScalePar
+        m$TrScaled <- sweep(m$Tr, 2, m$TrScalePar[1,]/m$TrScalePar[2,])
+        m
     }
+    ## parallelism would only slow-down (due to start-off time)
     hM1 <- lapply(parts, function(k) setHmsc(k, hM))
-
     ## STEP 2: sample Hmsc models for each nfolds * nChains case
+    chains <- length(hM$postList)
+    threads <- nfolds * chains
+    idfold <- rep(parts, each = chains)
+    seeds <- sample.int(.Machine$integer.max, threads)
+    ## to be called in parallel for each chain x fold, and
+    ## therefore we set nChains=1, nParallel=1 within
+    getSample <- function(i, hM1, ...) {
+        set.seed(seeds[i])
+        k <- idfold[i]
+        sampleMcmc(hM1[[k]], samples = hM$samples, thin = hM$thin,
+                   transient = hM$transient, adaptNf = hM$adaptNf,
+                   initPar = initPar, nChains = 1, nParallel = 1,
+                   updater = updater, verbose = verbose,
+                   alignPost = alignPost)
+    }
+    ## the next call can be made parallel
+    mods <- lapply(seq_len(threads), function(i, hM) getSample(i, hM1))
     ## STEP 3: combine predictions
     ## OUT
-    hM1
+    mods
 }
