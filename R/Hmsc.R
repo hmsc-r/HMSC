2cbd7c0973891593201a6fdc095997b826ebe5e7
Jari Oksanen <jhoksane@gmail.com>
2022-03-29 12:30:37 +0300

    report names of non-accepted XData data.frame variable types
    Matrix-variables in data.frames can occur, but are difficult to
    detect, and our reporting was really cryptic: now we at least
    report names of those variables that we do not accept. See
    issues #67 and #138.

--- R/Hmsc.R
+++ R/Hmsc.R
@@ -199,8 +199,8 @@ Hmsc = function(Y, XFormula=~., XData=NULL, X=NULL, XScale=TRUE,
         ## (sometimes they are character strings which is OK for most
         ## analyses but can fail in predict.Hmsc): DF is a data.frame
         OKvars <- function(DF) {
-            all(sapply(DF, function(a)
-                           !is.matrix(a) && (is.numeric(a) || is.factor(a))))
+            sapply(DF, function(a)
+                !is.matrix(a) && (is.numeric(a) || is.factor(a)))
         }
         if (inherits(XData, "list")) {
             if(length(XData) != hM$ns){
@@ -217,8 +217,9 @@ Hmsc = function(Y, XFormula=~., XData=NULL, X=NULL, XScale=TRUE,
             if(any(unlist(lapply(XData, function(a) nrow(a) != hM$ny)))){
                 stop("for each element of XData list the number of rows must be equal to the number of sampling units")
             }
-            if(!all(sapply(XData, function(a) OKvars(a))))
-                stop("all variables in XData list must be numeric or factors")
+            if(!all(ok <- sapply(XData, function(a) OKvars(a))))
+                stop("some XData variables were not numeric or factors",
+                     paste(names(ok)[!ok], collapse=", "))
             if(any(unlist(lapply(XData, function(a) any(is.na(a)))))){
                 stop("NA values are not allowed in XData")
             }
@@ -236,8 +237,9 @@ Hmsc = function(Y, XFormula=~., XData=NULL, X=NULL, XScale=TRUE,
             }
             ## check that all vars are numeric or factors (not, e.g.,
             ## characters)
-            if (!all(OKvars(XData)))
-                stop("all XData variables must be numeric or factors")
+            if (!all(ok <- OKvars(XData)))
+                stop("some XData variables were not numeric or factors: ",
+                     paste(names(ok)[!ok], collapse = ", "))
             ## check against derived classes of data.frame (such as tibble)
             if (class(XData)[1L] != "data.frame")
                 XData <- as.data.frame(XData, stringsAsFactors = TRUE)
