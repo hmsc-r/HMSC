883e2d66d87ea2637c71375ae6799a090a3de225
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2026-04-07 16:14:50 +0300

    Add experimental CORAL prior computationfor fast scalar phylogeny.
    CORAL prior variances are not computed correctly yet.

--- R/coralGetRareSpeciesPriors.R
+++ R/coralGetRareSpeciesPriors.R
@@ -9,6 +9,7 @@
 #' @param C.common.rare part of phylogeny similarity matrix between common (columns) and rare (rows) species
 #' @param interceptVarInflation multiplier for conditional variance prior of intercept term in the CORAL prior,
 #' compared to phylogeny-induced conditional prior
+#' @param phyloFast logical, if TRUE use fast tree traversal for phylogeny-induced priors
 #'
 #' @return
 #' A list containing CORAL prior as matrix of means and matrix of flattened covariances
@@ -20,47 +21,99 @@
 #'
 #' @export
 
-coralGetRareSpeciesPriors = function(m, spNames.rare, TrData.rare=NULL, phyloTree=NULL, C.common.rare=NULL, interceptVarInflation=5){
-   #TODO We shall avoid construction of C explicitely, as it is HUGE and SLOW once the number of rare is large.
-   # Best will be tweak the ape/phytools functionality somehow, but I am unaware of how that can be done.
-   # Alternatively, we need to disentangle the phylogeny tree structure manually to get these common-rare correlations.
-   # Can be done by searching the tip-closest common ancestor for each common-rare pair,
-   # but perhaps some vectorised solution is possible.
-
+coralGetRareSpeciesPriors = function(m, spNames.rare, TrData.rare=NULL, phyloTree=NULL, C.common.rare=NULL,
+                                     interceptVarInflation=5, phyloFast=FALSE, showProgress=FALSE){
    ns.rare = length(spNames.rare)
    if(is.null(TrData.rare)){
       Tr.rare = matrix(1, ns.rare, 1)
    } else{
       Tr.rare = model.matrix(m$TrFormula, TrData.rare)
    }
-   if(is.null(C.common.rare)){
+   if(phyloFast){
       if(is.null(phyloTree)) phyloTree = m$phyloTree
       spNames.all = c(m$spNames, spNames.rare)
-      phyloTree = keepTipRoot(phyloTree, spNames.all)
-      C = vcv(phyloTree, model="Brownian", corr=TRUE)
-      C12 = C[m$spNames, spNames.rare]
-   } else{
-      C12 = C.common.rare[m$spNames, spNames.rare]
+      ns.all = length(spNames.all)
+      # phyloTree = keepTipRoot(phyloTree, spNames.all)
+
+      treeList = vector("list", ns.all + phyloTree$Nnode)
+      for(i in 1:length(treeList)){
+         treeList[[i]] = list(n=0, child=NULL, edgeLen=NULL, parent=0, parentEdgeLen=0)
+      }
+      for(i in 1:nrow(phyloTree$edge)){
+         parentNode = phyloTree$edge[i,1]
+         childNodeOrig = phyloTree$edge[i,2]
+         if(childNodeOrig <= ns.all){
+            childNode = match(phyloTree$tip.label[childNodeOrig], spNames.all)
+         } else childNode = childNodeOrig
+         nodeObj = treeList[[parentNode]]
+         nodeObj$n = nodeObj$n + 1
+         nodeObj$child = c(nodeObj$child, childNode)
+         nodeObj$edgeLen = c(nodeObj$edgeLen, phyloTree$edge.length[i])
+         treeList[[parentNode]] = nodeObj
+         nodeObj = treeList[[childNode]]
+         nodeObj$parent = parentNode
+         nodeObj$parentEdgeLen = phyloTree$edge.length[i]
+         treeList[[childNode]] = nodeObj
+      }
+      phyloTreeRoot = setdiff(1:length(treeList), phyloTree$edge[,2])
+   } else {
+      if(is.null(C.common.rare)){
+         if(is.null(phyloTree)) phyloTree = m$phyloTree
+         spNames.all = c(m$spNames, spNames.rare)
+         # phyloTree = keepTipRoot(phyloTree, spNames.all)
+         C = vcv(phyloTree, model="Brownian", corr=TRUE)
+         C12 = C[m$spNames, spNames.rare]
+      } else{
+         C12 = C.common.rare[m$spNames, spNames.rare]
+      }
    }
+
    E = matrix(0, ns.rare, m$nc)
    E2 = matrix(0, ns.rare, m$nc^2)
    post = poolMcmcChains(m$postList)
    postN = length(post)
-   for(i in 1:postN){
+   if(showProgress == TRUE){
+      pb = txtProgressBar(max=postN, style=3)
+   }
+   for(i in seq_len(postN)){
       p = post[[i]]
       beta = p$Beta
       gamma = p$Gamma
       V = p$V
       rho = p$rho
-      Q11 = rho*m$C + (1-rho)*diag(m$ns)
-      iQ11 = chol2inv(chol(Q11))
       rbeta = beta - tcrossprod(gamma, m$Tr)
-      C21iQ11 = t(C12) %*% iQ11
-      muc = rho*C21iQ11%*%t(rbeta) + tcrossprod(Tr.rare, gamma)
-      mult = 1 - rho^2 * rowSums(C21iQ11 * t(C12))
+
+      if(phyloFast){
+         XTiDX = rep(0, ns.all)
+         XTiDS = matrix(0, m$nc, ns.all)
+         XTiDX[1:m$ns] = Inf
+         XTiDS[, 1:m$ns] = rbeta
+
+         # 1. Scalable conditional mean
+         if(rho > 0){
+            BetaLambdaCond = recFunScalar_opt(treeList, phyloTreeRoot, rho, XTiDX, XTiDS, sdMult=0)
+            muc = t(BetaLambdaCond[, (m$ns+1):ns.all]) + tcrossprod(Tr.rare, gamma)
+         } else{
+            muc = tcrossprod(Tr.rare, gamma)
+         }
+         # 2. Scalable mult calculation. Leave it as such for now
+         mult = 1 #computeScalableMult(treeList, phyloTreeRoot, rho, m$ns, ns.all)
+      } else {
+         Q11 = rho*m$C + (1-rho)*diag(m$ns)
+         iQ11 = chol2inv(chol(Q11))
+         C21iQ11 = t(C12) %*% iQ11
+         muc = rho*C21iQ11%*%t(rbeta) + tcrossprod(Tr.rare, gamma)
+         mult = 1 - rho^2 * rowSums(C21iQ11 * t(C12))
+      }
       E = E + muc
       vV = as.vector(V)
       E2 = E2 + muc[,rep(1:m$nc,each=m$nc)]*muc[,rep(1:m$nc,m$nc)] + matrix(mult,ns.rare,m$nc^2)*matrix(vV,ns.rare,m$nc^2,byrow=TRUE)
+      if(showProgress == TRUE){
+         setTxtProgressBar(pb, i)
+      }
+   }
+   if(showProgress == TRUE){
+      close(pb)
    }
    E = E/postN
    E2 = E2/postN
--- R/utilsFastPhylogeny.R
+++ R/utilsFastPhylogeny.R
@@ -66,7 +66,7 @@ fastPhyloBilinearDet = function(treeList, X, Y, root, iV, rho){
    return(list(XiSY=res$XiSY, logDet=res$logDet))
 }
 
-recFunSampleUp = function(node, treeList, iV, rho, rho2Mat, XTiDX, XTiDS, diV2x){
+recFunSampleUp_v1 = function(node, treeList, iV, rho, rho2Mat, XTiDX, XTiDS){
    nc = nrow(iV)
    nChild = treeList[[node]]$n
    edgeLenVec = treeList[[node]]$edgeLen
@@ -75,7 +75,7 @@ recFunSampleUp = function(node, treeList, iV, rho, rho2Mat, XTiDX, XTiDS, diV2x)
    for(i in seq_len(nChild)){
       childNode = treeList[[node]]$child[i]
       if(treeList[[childNode]]$n > 0){
-         res = recFunSampleUp(childNode, treeList, iV, rho, rho2Mat, XTiDX, XTiDS, diV2x)
+         res = recFunSampleUp(childNode, treeList, iV, rho, rho2Mat, XTiDX, XTiDS)
          iSigmaChild_m[,i] = res$iSigma_m
          iSigmaChild[,,i] = res$iSigma
          treeList = res$treeList
@@ -96,6 +96,44 @@ recFunSampleUp = function(node, treeList, iV, rho, rho2Mat, XTiDX, XTiDS, diV2x)
       iRWT_D1_iSigmaChild = backsolve(RW, D1_iSigmaChild, transpose=TRUE)
       iSigmaHat[,,i] =  iSigmaChild[,,i] - edgeLenVec[i] * crossprod(iRWT_D1_iSigmaChild)
       iSigmaHat_m[,i] = iSigmaChild_m[,i] - edgeLenVec[i] * crossprod(iRWT_D1_iSigmaChild, backsolve(RW, sqrt(rho)*iSigmaChild_m[,i], transpose=TRUE))
+   }
+   iSigmaTilde = rowSums(iSigmaHat, dims=2)
+   iSigmaTilde_mTilde = rowSums(iSigmaHat_m)
+   treeList[[node]]$iSm <- iSigmaTilde_mTilde
+   treeList[[node]]$iS <- iSigmaTilde
+   return(list(iSigma_m=iSigmaTilde_mTilde, iSigma=iSigmaTilde, treeList=treeList))
+}
+
+recFunSampleUp = function(node, treeList, iV, rho, rho2Mat, XTiDX, XTiDS){
+   nc = nrow(iV)
+   nChild = treeList[[node]]$n
+   edgeLenVec = treeList[[node]]$edgeLen
+   iSigmaHat_m = matrix(NA,nc,nChild)
+   iSigmaHat = array(NA, c(nc,nc,nChild))
+   for(i in seq_len(nChild)){
+      childNode = treeList[[node]]$child[i]
+      if(treeList[[childNode]]$n > 0){
+         res = recFunSampleUp(childNode, treeList, iV, rho, rho2Mat, XTiDX, XTiDS)
+         iSigmaChild_m = res$iSigma_m
+         iSigmaChild = res$iSigma
+         treeList = res$treeList
+         rhoEff = edgeLenVec[i] * rho
+      } else{
+         treeList[[childNode]]$iS <- (iSigmaChild = XTiDX[,,childNode])
+         treeList[[childNode]]$iSm <- (iSigmaChild_m = XTiDS[,childNode])
+         rhoEff = edgeLenVec[i] * rho + rho2Mat[,childNode]
+      }
+      if(all(diag(iSigmaChild) == Inf)){
+         iSigmaHat[,,i] = rhoEff^-1 * iV
+         iSigmaHat_m[,i] = iSigmaHat[,,i] %*% iSigmaChild_m
+      } else{
+         D_iSigmaChild = matmulDiagMat(sqrt(rhoEff), iSigmaChild)
+         W = iV + matmulMatDiag(D_iSigmaChild, sqrt(rhoEff))
+         RW = chol(W)
+         iRWT_D_iSigmaChild = backsolve(RW, D_iSigmaChild, transpose=TRUE)
+         iSigmaHat[,,i] = iSigmaChild - crossprod(iRWT_D_iSigmaChild)
+         iSigmaHat_m[,i] = iSigmaChild_m - crossprod(iRWT_D_iSigmaChild, backsolve(RW, sqrt(rhoEff)*iSigmaChild_m, transpose=TRUE))
+      }
    }
    iSigmaTilde = rowSums(iSigmaHat, dims=2)
    iSigmaTilde_mTilde = rowSums(iSigmaHat_m)
@@ -127,10 +165,10 @@ recFunSampleDown = function(node, treeList, V, rho, rho2Mat, sdMult){
          W = edgeLenVec[i] * V1 + matmulDiagMat(sqrt(rho2), matmulMatDiag(V, sqrt(rho2)))
          RW = chol(W)
          iSigma = iS + chol2inv(RW)
-         iSigma_mu = iSm + backsolve(RW, backsolve(RW,beta,transpose=TRUE))
+         iSigma_mu = iSm + backsolve(RW, backsolve(RW, beta, transpose=TRUE))
          RiSigma = chol(iSigma)
          BetaRanPart = backsolve(RiSigma, rnorm(nc))
-         treeList[[childNode]]$beta <- backsolve(RiSigma, backsolve(RiSigma,iSigma_mu,transpose=TRUE)) + sdMult*BetaRanPart
+         treeList[[childNode]]$beta <- backsolve(RiSigma, backsolve(RiSigma, iSigma_mu, transpose=TRUE)) + sdMult*BetaRanPart
       }
    }
    return(treeList)
@@ -153,5 +191,216 @@ recFunSample = function(treeList, root, V, iV, rho, rho2Mat, XTiDX, XTiDS, sdMul
    for(j in 1:ns){
       Beta[,j] = treeListTemp[[j]]$beta
    }
+   return(Beta)
+}
+
+
+
+
+recFunScalarUp = function(node, treeList, rho, XTiDX, XTiDS){
+   nc = nrow(XTiDS)
+   nChild = treeList[[node]]$n
+   edgeLenVec = treeList[[node]]$edgeLen
+   iSigmaHat_m = matrix(NA, nc, nChild)
+   iSigmaHat = rep(NA, nChild)
+   for(i in seq_len(nChild)){
+      childNode = treeList[[node]]$child[i]
+      if(treeList[[childNode]]$n > 0){
+         res = recFunScalarUp(childNode, treeList, rho, XTiDX, XTiDS)
+         iSigmaChild_m = res$iSigma_m
+         iSigmaChild = res$iSigma
+         treeList = res$treeList
+         rhoEff = edgeLenVec[i] * rho
+      } else{
+         treeList[[childNode]]$iS <- (iSigmaChild = XTiDX[childNode])
+         treeList[[childNode]]$iSm <- (iSigmaChild_m = XTiDS[,childNode])
+         rhoEff = edgeLenVec[i] * rho + (1 - rho)
+      }
+      if(iSigmaChild == Inf){
+         iSigmaHat[i] = rhoEff^-1
+         iSigmaHat_m[,i] = iSigmaHat[i] * iSigmaChild_m
+      } else if(iSigmaChild == 0){
+         iSigmaHat[i] = 0
+         iSigmaHat_m[,i] = iSigmaChild_m
+      } else{
+         iSigmaHat[i] = (iSigmaChild^-1 + rhoEff)^-1
+         iSigmaHat_m[,i] = (iSigmaChild_m / iSigmaChild) * iSigmaHat[i]
+      }
+   }
+   iSigmaTilde = sum(iSigmaHat)
+   iSigmaTilde_mTilde = rowSums(iSigmaHat_m)
+   treeList[[node]]$iSm <- iSigmaTilde_mTilde
+   treeList[[node]]$iS <- iSigmaTilde
+   return(list(iSigma_m=iSigmaTilde_mTilde, iSigma=iSigmaTilde, treeList=treeList))
+}
+
+recFunScalarDown = function(node, treeList, rho, sdMult){
+   # nc = nrow(V)
+   nChild = treeList[[node]]$n
+   edgeLenVec = treeList[[node]]$edgeLen
+   beta = treeList[[node]]$beta
+   for(i in seq_len(nChild)){
+      childNode = treeList[[node]]$child[i]
+      iS = treeList[[childNode]]$iS
+      iSm = treeList[[childNode]]$iSm
+      if(treeList[[childNode]]$n > 0){
+         rhoEff = edgeLenVec[i] * rho
+      } else{
+         rhoEff = edgeLenVec[i] * rho + (1 - rho)
+      }
+      if(iS == Inf){
+         mu = iSm
+      } else{
+         isigma2 = rhoEff^-1 + iS
+         mu = (iSm + beta / rhoEff) / isigma2
+      }
+      
+      treeList[[childNode]]$beta <- mu #+ sdMult*BetaRanPart
+      if(treeList[[childNode]]$n > 0){
+         treeList = recFunScalarDown(childNode, treeList, rho, sdMult)
+      }
+   }
+   return(treeList)
+}
+
+recFunScalar = function(treeList, root, rho, XTiDX, XTiDS, sdMult=1){
+   ns = ncol(XTiDS)
+   nc = nrow(XTiDS)
+   treeListTemp = treeList
+   for(i in 1:length(treeListTemp)){
+      treeListTemp[[i]]$iS = NULL
+      treeListTemp[[i]]$iSm = NULL
+      treeListTemp[[i]]$beta = NULL
+   }
+   resUp = recFunScalarUp(root, treeListTemp, rho, XTiDX, XTiDS)
+   treeListTemp = resUp$treeList
+   treeListTemp[[root]]$beta = matrix(0, nc, 1)
+   treeListTemp = recFunScalarDown(root, treeListTemp, rho, sdMult)
+   Beta = matrix(NA, nc, ns)
+   for(j in 1:ns){
+      Beta[,j] = treeListTemp[[j]]$beta
+   }
+   return(Beta)
+}
+
+
+
+recFunScalarUp_opt = function(node, envTree, rho, XTiDX, XTiDS){
+   nodeEnv = envTree[[node]]
+   nChild = nodeEnv$n
+   edgeLenVec = nodeEnv$edgeLen
+   
+   # OPTIMIZATION: Instead of allocating iSigmaHat_m (matrix) and iSigmaHat (vector) 
+   # just to sum them later, we initialize running accumulators. This prevents 
+   # thousands of small matrix allocations during recursion.
+   iSigmaTilde = 0
+   iSigmaTilde_mTilde = numeric(nrow(XTiDS))
+   
+   for(i in seq_len(nChild)){
+      childIdx = nodeEnv$child[i]
+      childEnv = envTree[[childIdx]]
+      
+      if(childEnv$n > 0){
+         res = recFunScalarUp_opt(childIdx, envTree, rho, XTiDX, XTiDS)
+         iSigmaChild_m = res$iSigma_m
+         iSigmaChild = res$iSigma
+         rhoEff = edgeLenVec[i] * rho
+      } else {
+         # Leaf node: modify environment in-place
+         iSigmaChild = XTiDX[childIdx]
+         iSigmaChild_m = XTiDS[, childIdx]
+         
+         childEnv$iS <- iSigmaChild
+         childEnv$iSm <- iSigmaChild_m
+         
+         rhoEff = edgeLenVec[i] * rho + (1 - rho)
+      }
+      
+      # Calculate current child's contribution
+      if(iSigmaChild == Inf){
+         iSigmaHat_i = rhoEff^-1
+         iSigmaHat_m_i = iSigmaHat_i * iSigmaChild_m
+      } else if(iSigmaChild == 0){
+         iSigmaHat_i = 0
+         iSigmaHat_m_i = iSigmaChild_m
+      } else {
+         iSigmaHat_i = (iSigmaChild^-1 + rhoEff)^-1
+         iSigmaHat_m_i = (iSigmaChild_m / iSigmaChild) * iSigmaHat_i
+      }
+      
+      # Accumulate running sums directly
+      iSigmaTilde = iSigmaTilde + iSigmaHat_i
+      iSigmaTilde_mTilde = iSigmaTilde_mTilde + iSigmaHat_m_i
+   }
+   
+   # Update parent environment in-place
+   nodeEnv$iSm <- iSigmaTilde_mTilde
+   nodeEnv$iS <- iSigmaTilde
+   
+   return(list(iSigma_m = iSigmaTilde_mTilde, iSigma = iSigmaTilde))
+}
+
+recFunScalarDown_opt = function(node, envTree, rho, sdMult){
+   nodeEnv = envTree[[node]]
+   nChild = nodeEnv$n
+   edgeLenVec = nodeEnv$edgeLen
+   beta = nodeEnv$beta
+   
+   for(i in seq_len(nChild)){
+      childIdx = nodeEnv$child[i]
+      childEnv = envTree[[childIdx]]
+      
+      iS = childEnv$iS
+      iSm = childEnv$iSm
+      
+      if(childEnv$n > 0){
+         rhoEff = edgeLenVec[i] * rho
+      } else {
+         rhoEff = edgeLenVec[i] * rho + (1 - rho)
+      }
+      
+      if(iS == Inf){
+         mu = iSm
+      } else {
+         isigma2 = rhoEff^-1 + iS
+         mu = (iSm + beta / rhoEff) / isigma2
+      }
+      
+      # Update beta in-place
+      childEnv$beta <- mu 
+      
+      if(childEnv$n > 0){
+         recFunScalarDown_opt(childIdx, envTree, rho, sdMult)
+      }
+   }
+   return(invisible(NULL))
+}
+
+recFunScalar_opt = function(treeList, root, rho, XTiDX, XTiDS, sdMult=1){
+   ns = ncol(XTiDS)
+   nc = nrow(XTiDS)
+   
+   # Convert to environments (pass-by-reference).
+   # OPTIMIZATION: Skipped the loop that NULLs out variables. It is unnecessary 
+   # because the Up/Down passes will strictly overwrite them anyway.
+   envTree = lapply(treeList, function(x) list2env(x, parent = emptyenv()))
+   
+   # Upward Pass
+   recFunScalarUp_opt(root, envTree, rho, XTiDX, XTiDS)
+   
+   # Downward Pass Setup
+   envTree[[root]]$beta = matrix(0, nc, 1)
+   
+   # Downward Pass
+   recFunScalarDown_opt(root, envTree, rho, sdMult)
+   
+   # Extract final results
+   # OPTIMIZATION: Initializing with NA_real_ avoids implicit type coercion 
+   # from logical NA to double when populating the matrix.
+   Beta = matrix(NA_real_, nc, ns)
+   for(j in seq_len(ns)){
+      Beta[,j] = envTree[[j]]$beta
+   }
+   
    return(Beta)
 }
